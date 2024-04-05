d94682775765bd729712094337a03551ce812e10
iman <imanibrahim301@gmail.com>
2024-04-05 16:28:09 +0300

    added Lab2

---
+++ Widget_Excercise/main.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/card.dart';
+import 'package:flutter_application_2/listView2.dart';
+import 'package:flutter_application_2/tracker.dart';
+import 'excersise3.dart';
+import 'package:flutter_application_2/excersie4.dart';
+import 'font.dart';
+import 'img.dart';
+import 'notNeed.dart';
+import 'next1.dart';
+import 'listView.dart';
+import 'stack.dart';
+import 'card.dart';
+import 'listView2.dart';
+import 'textField.dart';
+import 'chip.dart';
+import 'checkBoxSlider.dart';
+import 'order.dart';
+import 'searchProduct.dart';
+
+
+void main() {
+  runApp(Lab());
+}
+
+class Lab extends StatelessWidget {
+  // const Lab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: statelessWidget11('')
+      );
+        }
+      }
+
+
+// flutter pub get google
